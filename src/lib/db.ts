d3c70c18d123555d8595e52176a7c5e333a42919
kaushikumesh2001 <kaushikumesh2001@gmail.com>
2026-01-16 14:23:07 +0530

    my-notes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,21 @@
-import { Pool } from "pg";  
+// import { Pool } from "pg";  
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "notes_db",
+//   password: "postgres",
+//   port: 5432,
+// });
+
+// export default pool;
+
+
+import { Pool } from "pg";
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "notes_db",
-  password: "postgres",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
